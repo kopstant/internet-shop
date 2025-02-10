9edf0a786fdc5ce80a2d94faf22cbfefd761a918
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-02-10 11:42:42 -0400

    Модифицированы вывод и обработка запросов

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse
 
 from blog.models import Post
 
@@ -8,9 +9,18 @@ from blog.models import Post
 class PostListView(ListView):  # Отображает список объектов. Основной метод get.
     model = Post
 
+    def get_queryset(self):
+        return Post.objects.filter(publications_flag=True)
+
 
 class PostDetailView(DetailView):  # Отображает детали конкретного объекта. Основной метод - get.
     model = Post
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        obj.numbers_of_posts += 1
+        obj.save()
+        return obj
 
 
 class PostCreateView(CreateView):  # Создает новый объект. Методы - get, post, form_valid.
@@ -23,6 +33,9 @@ class PostUpdateView(UpdateView):  # Обновляет существующий
     model = Post
     fields = ("title", "content", "preview", "publications_flag")
     success_url = reverse_lazy('blog:post_list')
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', args=[self.kwargs.get('pk')])
 
 
 class PostDeleteView(DeleteView):  # Удаляет объект. Методы - get, post
