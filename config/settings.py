9b6646e2f1587976424c9f24ec303bff1ea7041e
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-02-15 22:36:21 -0400

    Обновил настройки, рассылка при регистрации работает корректно.

--- config/settings.py
+++ config/settings.py
@@ -98,13 +98,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND'),
-EMAIL_HOST = os.getenv('EMAIL_HOST'),
-EMAIL_PORT = os.getenv('EMAIL_PORT'),
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS'),
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL'),
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER'),
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD'),
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.yandex.ru'
+EMAIL_PORT=465
+EMAIL_USE_TLS=False
+EMAIL_USE_SSL=True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_URL = 'users:login'
